f9c65871cbea2a676ed14109cb90978ba0880547
german gomez <germangb42@gmail.com>
2016-05-05 16:34:00 +0000

    evaluation

--- testbed.py
+++ testbed.py
@@ -8,7 +8,7 @@ try:
 except:
     path = 'test/car.png'
 
-cap = cv2.VideoCapture('entrada3.avi')
+cap = cv2.VideoCapture('2.mp4')
 mask = cv2.imread('mask.png')
 mask = np.double(cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)) / 255
 
@@ -34,17 +34,19 @@ while True:
         continue
     frame_count = 0
 
-    image = image[100:580, 300-200:940-200]
+    image = image[0:480, 0:640];
+    #image = image[100:580, 300-200:940-200]
+    #image = image[100:500, 0:640]
     raw = image.copy()
 
     # mask image
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     gray = np.double(gray)
-    gray = cv2.multiply(gray, mask);
+    gray = cv2.multiply(gray, mask)
     gray = np.uint8(gray)
 
     # get a list of potential license plates
-    segments = localize.get_plate_regions(gray)
+    segments = localize.get_plate_regions(gray, debug=image)
     localize.get_plate_regions(gray)
 
     if len(segments) == 1:
@@ -58,18 +60,15 @@ while True:
         if abs(x - h_x) > 32 or abs(y - h_y) > 32 or abs(w - h_w) > 32 or abs(h - h_h) > 32:
             h_x, x_y, h_w, h_h = x, y, w, h
 
+            #if len(plates) == 8:
+                #plates = []
+                #height = 0;
+
             plates.append(image[y:y + h, x:x + w])
             total_im = np.zeros((480, 640, 3), dtype="uint8")
             total_im[y:y + h, x:x + w] = raw[y:y+h, x:x+w]
 
-            #src = np.array([[0, -50], [640, 0+50], [640, 480+50], [0, 480-50]], dtype="float32")
-            #dst = np.array([[0, 0], [640, 0], [640, 480], [0, 480]], dtype="float32")
-            #M = cv2.getPerspectiveTransform(src, dst)
-
-            #output = np.array((640, 480, 3), dtype="uint8")
-            #cv2.perspectiveTransform(total_im, M, output)
-
-            cv2.imwrite("more_plates/plate{}.jpg".format(len(plates)), raw[y:y + h, x:x + w])
+            cv2.imwrite("test_plates/plate{}.jpg".format(len(plates)), raw[y:y + h, x:x + w])
             width = max(width, w)
             height += h
 
@@ -82,7 +81,7 @@ while True:
             acum += shape[0]
             cv2.imshow("licenses", licens)
 
-    # output image
+    # output imageheight
     cv2.imshow("output", image)
     if cv2.waitKey(1) & 255 == ord('q'):
         break
